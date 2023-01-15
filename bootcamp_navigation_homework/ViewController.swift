ca114522579aa1d519179baa1b1455f8bb70897f
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2023-01-15 21:53:03 +0300

    Initial Commit

---
+++ bootcamp_navigation_homework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bootcamp_navigation_homework
+//
+//  Created by Yılmaz Yağız Dokumacı on 15.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
